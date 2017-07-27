2e1550bcdbe0be7d129aa4b49ef47a63ac44de6a
Max Frederiksen <max.frederiksencoles@gmail.com>
2017-07-27 12:12:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	$("#quoteButton").on("click", function(){
+		
+		$.ajaxSetup({cache:false});
+    
+    
+    	$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(quote) {
+    		$(".quoteHolder").html(' &ldquo; ' + quote[0].content + ' &rdquo; ');
+    		$(".author").html(" - " + quote[0].title);
+    		console.log(quote);
+
+    	});
+    });
+
+    $("#twitterLink").click(function(){
+    	var quoteTweet = $('.quoteHolder').text() + $('.author').text() + ' #quotes';
+
+    	var tweetLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quoteTweet);
+    	window.open(tweetLink, '_blank');
+
+    	});
+});
